ae7d742a3c232290140877232d8f35085a9a4d3c
LOSTsage <devat272@gmail.com>
2026-02-11 22:46:03 +0530

    completed the basic home page

--- protfolio/src/App.jsx
+++ protfolio/src/App.jsx
@@ -41,22 +41,34 @@ function App() {
     <AnimatePresence>
       <motion.div className="myNameDiv">
         {" "}
-        <h1>Hi I'm</h1>{" "}
-        <motion.h1 key={'h1'}
+        <h2>Hi I'm</h2>{" "}
+        <motion.h2 key={'h1'}
           className="name"
           initial={{ width: 0 }}
           animate={{ width: "fit-content" }}
-          transition={{ width: { duration: 1, ease: "easeOut" } }}
+          transition={{ width: { duration: 0.5, ease: "easeIn" } }}
         >
           Devansh
-        </motion.h1>
+        </motion.h2>
+        
         <motion.span key={'ani'}
           className="ani"
           initial={{ opacity: 1 }}
           animate={{ opacity: 0 }}
-          transition={{ repeat: 2, duration: 0.4, ease: "easeInOut" }}
-        ></motion.span>{" "}
+          transition={{ repeat: 0, duration: 1, ease: "easeInOut" }}
+        ></motion.span>
       </motion.div>
+       <motion.div key={'lp'}
+        className="headTitleDiv"
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.7, ease: "easeOut" ,delay:0.7}}
+      >
+        <h1 className="headTitle">Frontend Devloper</h1>
+       
+      </motion.div>
+      
+        
       <motion.div
         key="yo"
         onMouseOut={() => {
@@ -74,16 +86,12 @@ function App() {
           alt="my img"
         />
       </motion.div>
-      <motion.div key={'lp'}
-        className="headTitleDiv"
-        initial={{ y: 20, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.7, ease: "easeOut" }}
-      >
-        <h1 className="headTitle">Frontend</h1>
-        <h1 className="headTitle"> Devloper</h1>
-        <p></p>
-      </motion.div>
+     <p className="discription">I’m a frontend developer focused on building responsive and interactive web interfaces using React and modern tools.
+I care about performance, clean architecture, and smooth user experiences.
+Currently working on improving animation-driven UI and real-world project quality.</p>
+<div className="viewProjectButtonDiv">
+<button className="viewProjectButton">View project</button>
+</div>
     </AnimatePresence>
   );
 }
